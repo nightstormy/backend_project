a7b9a585f3ff73a107cf2394d546bb606e533fbd
Luis Salazar <41168258+nightstormy@users.noreply.github.com>
2020-02-28 12:36:56 -0400

    stash

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ Route::prefix('v1')->name('v1.')->namespace('v1\Auth')->group(function () {
     Route::post('/register', 'UserController@register')->name('register');
     Route::get('/prueba', 'UserController@prueba')->name('prueba');
    
-
     Route::group(['middleware' => 'auth:api'], function () {
         Route::post('details', 'UserController@details')->name('details');
     });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('v1')->name('v1.')->namespace('v1\Auth')->group(function () {
+    Route::post('/login', 'UserController@login')->name('login');
+    Route::post('/register', 'UserController@register')->name('register');
+    Route::get('/prueba', 'UserController@prueba')->name('prueba');
+   
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('details', 'UserController@details')->name('details');
+    });
+});
